fe52086de50742116f7f95d1664334e2ab752f57
sugiyama.t <sugiyama.t@water-cell.jp>
2021-03-20 02:37:04 +0900

    lint周りの設定を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        node: true,
     },
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
@@ -21,5 +23,8 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "react/prop-types": "off"
     }
 };
