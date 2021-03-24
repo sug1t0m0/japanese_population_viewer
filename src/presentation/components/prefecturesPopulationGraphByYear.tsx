59e55aeea32c91fc82310eb6f3e05587d4a04d06
sugiyama.t <sugiyama.t@water-cell.jp>
2021-03-24 13:29:24 +0900

    x軸 と y軸に単位をつける

--- src/presentation/components/prefecturesPopulationGraphByYear.tsx
+++ src/presentation/components/prefecturesPopulationGraphByYear.tsx
@@ -21,10 +21,10 @@ export const PrefecturesPopulationGraphByYear = (props: Props) => {
       {props.selectedPrefCodeList.length === 0 && <p>人口を表示する県を選択してください</p>}
       {props.prefecturesPopulationData.isLoading && <p>ロード中</p>}
       {props.selectedPrefCodeList.length > 0 && !props.prefecturesPopulationData.isLoading && (
-        <LineChart width={730} height={250} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <LineChart width={730} height={250} data={data} margin={{ top: 5, right: 30, left: 50, bottom: 5 }}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="year" />
-          <YAxis />
+          <XAxis dataKey="year" unit="年" />
+          <YAxis unit="人" />
           <Tooltip />
           <Legend />
           {selectedPrefNames.map((selectedPrefName, i) => (
