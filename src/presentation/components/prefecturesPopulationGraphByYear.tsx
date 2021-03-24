6b98e664369c7f98f28d11c39c17081e1e49f95b
sugiyama.t <sugiyama.t@water-cell.jp>
2021-03-25 00:19:54 +0900

    UiStackWrapper を取り入れてみる

--- src/presentation/components/prefecturesPopulationGraphByYear.tsx
+++ src/presentation/components/prefecturesPopulationGraphByYear.tsx
@@ -3,6 +3,7 @@ import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'r
 import { PrefecturesData } from '../../domain/prefecture'
 import { PrefecturesPopulationData } from '../../domain/prefecturesPopulation'
 import { convertFromPrefecturesPopulationData } from '../../domain/prefecturesPopulationGraphByYear'
+import { defaultProps, UiStackWrapper } from './uiStackWrapper'
 
 type Props = {
   selectedPrefCodeList: number[]
@@ -17,21 +18,27 @@ export const PrefecturesPopulationGraphByYear = (props: Props) => {
   const colors = ['#C55859', '#F08C57', '#F2DA48', '#48C176', '#4C9CD7', '#8A69B6']
 
   return (
-    <>
-      {props.selectedPrefCodeList.length === 0 && <p>人口を表示する県を選択してください</p>}
-      {props.prefecturesPopulationData.isLoading && <p>ロード中</p>}
-      {props.selectedPrefCodeList.length > 0 && !props.prefecturesPopulationData.isLoading && (
-        <LineChart width={730} height={250} data={data} margin={{ top: 5, right: 30, left: 50, bottom: 5 }}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="year" unit="年" />
-          <YAxis unit="人" />
-          <Tooltip />
-          <Legend />
-          {selectedPrefNames.map((selectedPrefName, i) => (
-            <Line key={i} dataKey={selectedPrefName} stroke={colors[i % colors.length]} />
-          ))}
-        </LineChart>
-      )}
-    </>
+    <UiStackWrapper
+      {...{
+        ...defaultProps,
+        isError: props.prefecturesPopulationData.isError || props.prefecturesData.isError,
+        isLoading: props.prefecturesPopulationData.isLoading || props.prefecturesData.isLoading,
+        isBlank: props.selectedPrefCodeList.length === 0,
+        loadingComponent: <p>ロード中</p>,
+        blankComponent: <p>人口を表示する県を選択してください</p>,
+        idealComponent: (
+          <LineChart width={730} height={250} data={data} margin={{ top: 5, right: 30, left: 50, bottom: 5 }}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="year" unit="年" />
+            <YAxis unit="人" />
+            <Tooltip />
+            <Legend />
+            {selectedPrefNames.map((selectedPrefName, i) => (
+              <Line key={i} dataKey={selectedPrefName} stroke={colors[i % colors.length]} />
+            ))}
+          </LineChart>
+        ),
+      }}
+    />
   )
 }
